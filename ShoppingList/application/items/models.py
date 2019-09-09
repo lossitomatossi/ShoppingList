d2267a75a95971b4fba234ebe24bc21f30c685de
lossitomatossi <tomas.terala@gmail.com>
2019-09-09 14:41:25 +0300

    2.2 asti osat

---
+++ ShoppingList/application/items/models.py
@@ -0,0 +1,17 @@
+from application import db
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
+    onupdate=db.func.current_timestamp())
+
+    name = db.Column(db.String(144), nullable=False)
+    amount = db.Column(db.Integer, nullable=True)
+    bought = db.Column(db.Boolean, nullable=False)
+    category = db.Column(db.String(40), nullable=False)
+
+    def __init__(self, name):
+        self.name = name
+        self.amount = amount
+        self.bought = False
