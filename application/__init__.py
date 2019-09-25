3bf4106d1bf27e66ee63f2038d7053c1440cb9cc
lossitomatossi <tomas.terala@gmail.com>
2019-09-25 18:52:42 +0300

    korjattu init.py kirjotusvirhe

--- application/__init__.py
+++ application/__init__.py
@@ -13,7 +13,7 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 if os.environ.get("HEROKU"):
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URI")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 else:
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///items.db"
     app.config["SQLALCHEMY_ECHO"] = True
