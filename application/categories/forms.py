2043beb4d18ae2e5524f05f125cfe550e357ae0d
lossitomatossi <tomas.terala@gmail.com>
2019-09-26 23:56:01 +0300

    korjattu kopionnista tullut väärä tieto

--- application/categories/forms.py
+++ application/categories/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, validators
 
 class CategoryForm(FlaskForm):
-    name = StringField("Item name", [validators.Length(min=1)])
+    name = StringField("Category name", [validators.Length(min=1)])
 
     class Meta:
         csrf = False
