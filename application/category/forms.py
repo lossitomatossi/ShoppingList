2340eb9309dd37617964c7b359c227e4f7439e42
lossitomatossi <tomas.terala@gmail.com>
2019-09-26 22:32:28 +0300

    lisättiin category.forms.py versionhallintaan

--- application/category/forms.py
+++ application/category/forms.py
@@ -3,7 +3,6 @@ from wtforms import StringField, validators
 
 class CategoryForm(FlaskForm):
     name = StringField("Item name", [validators.Length(min=1)])
-    more_info = StringField("Category name")
 
     class Meta:
         csrf = False
