1d2102fdd4a767f3f19857d0fc8ef508ae08b860
lossitomatossi <tomas.terala@gmail.com>
2019-09-26 22:58:47 +0300

    poistettiin unohtunut category attribuutti

--- application/items/models.py
+++ application/items/models.py
@@ -5,7 +5,6 @@ class Item(Base):
     name = db.Column(db.String(144), nullable=False)
     amount = db.Column(db.Integer, nullable=True)
     bought = db.Column(db.Boolean, nullable=False)
-    category = db.Column(db.String(40), nullable=False)
 
     account_id = db.Column(db.Integer, db.ForeignKey('account.id'),
                            nullable=False)
