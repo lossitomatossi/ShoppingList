ec204f9c93f6c4ed830fbf91163fda2f9512c4c5
lossitomatossi <tomas.terala@gmail.com>
2019-10-10 18:32:32 +0300

    items.new.html muotoilu aloitettu(ylempi formi ei toimi)

--- application/items/forms.py
+++ application/items/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, IntegerField, validators
 
 class ItemForm(FlaskForm):
-    name = StringField("Item name", [validators.Length(min=2)])
+    name = StringField("Name", [validators.Length(min=2)])
     bought = BooleanField("Bought")
     amount = IntegerField("Amount")
 
