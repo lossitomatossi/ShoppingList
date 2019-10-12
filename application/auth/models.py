c6b2d3457d435556c65c300eed9bdaf8bf13d282
lossitomatossi <tomas.terala@gmail.com>
2019-10-12 21:37:49 +0300

    lisätty mahdollisuus rooleille

--- application/auth/models.py
+++ application/auth/models.py
@@ -30,6 +30,9 @@ class User(Base):
     def is_authenticated(self):
         return True
 
+    def roles(self):
+        return ["ADMIN"]
+
 
     @staticmethod
     def find_users_with_no_items(bought=True):
