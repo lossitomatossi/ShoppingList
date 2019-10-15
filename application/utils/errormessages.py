ff7f9578fd02be483511cacedfd1b89da13c3842
lossitomatossi <tomas.terala@gmail.com>
2019-10-16 01:02:50 +0300

    lisätty utils kansio ja errormessages tiedosto

---
+++ application/utils/errormessages.py
@@ -0,0 +1,10 @@
+import os
+
+msg_username_length = "Username must be between 1 and 144 characters"
+msg_password_match = "Passwords do not match"
+msg_password_length = "Passwords must be between 1 and 144 characters"
+msg_username_length = "Username must be between 1 and 144 characters"
+
+msg_name_length = "Name must be between 1 and 144 characters"
+
+msg_item_amount = "Amount must be given (can be 0(zero))"
