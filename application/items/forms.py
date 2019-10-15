daac96cde2a7e3520a0031a3562e3ed201678940
lossitomatossi <tomas.terala@gmail.com>
2019-10-16 00:09:56 +0300

    lisätty validointeja items.forms.py

--- application/items/forms.py
+++ application/items/forms.py
@@ -1,21 +1,22 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, IntegerField, SelectField, validators
+from wtforms.validators import ValidationError, InputRequired, EqualTo, Length
 from application.categories.models import Category
 
 class ItemForm(FlaskForm):
-    name = StringField("Name", [validators.Length(min=2)])
-    bought = BooleanField("Bought")
-    amount = IntegerField("Amount")
+    name = StringField("Name", [InputRequired(), Length(min=1,max=144)])
+    bought = BooleanField("Bought", [InputRequired()])
+    amount = IntegerField("Amount", [InputRequired()])
 
     class Meta:
         csrf = False
 
 
 class ItemCreateForm(FlaskForm):
-    name = StringField("Name", [validators.Length(min=2)])
-    bought = BooleanField("Bought")
-    amount = IntegerField("Amount")
-    category = IntegerField("Category")
+    name = StringField("Name", [InputRequired(), Length(min=2)])
+    bought = BooleanField("Bought", [InputRequired()])
+    amount = IntegerField("Amount", [InputRequired()])
+    category = IntegerField("Category", [InputRequired()])
 
     class Meta:
         crsf = False
