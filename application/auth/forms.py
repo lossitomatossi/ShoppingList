ef2cd4a5477c835a4839a0f4deb63ac1f5dc8ee7
lossitomatossi <tomas.terala@gmail.com>
2019-10-17 17:22:55 +0300

    auth validointeja ja userprofile.html siistimistä

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -1,28 +1,28 @@
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, validators
-from wtforms.validators import ValidationError, InputRequired, EqualTo, Length
+from wtforms.validators import ValidationError, InputRequired, EqualTo, Length, DataRequired
 from application.utils.errormessages import msg_username_length, msg_password_match, msg_password_length, msg_name_length
 
 class LoginForm(FlaskForm):
-    username = StringField("Username", [InputRequired(), Length(min=1, max=144, message=msg_username_length)])
-    password = PasswordField("Password", [InputRequired(), Length(min=1, max=144, message=msg_password_length)])
+    username = StringField("Username", [InputRequired(), Length(min=1, max=144, message=msg_username_length), DataRequired(message="Only using spaces not allowed")])
+    password = PasswordField("Password", [InputRequired(), Length(min=1, max=144, message=msg_password_length), DataRequired(message="Only using spaces not allowed")])
 
     class Meta:
         csrf = False
 
 
 class SignupForm(FlaskForm):
-    name = StringField("Name", [InputRequired(), Length(min=1, max=144, message=msg_name_length)])
-    username = StringField("Username", [InputRequired(), Length(min=1, max=144, message=msg_username_length)])
-    password = PasswordField("Password", [validators.required(), validators.Length(min=2)])
-    password2 = PasswordField("Password", [validators.required(), validators.Length(min=2)])
+    name = StringField("Name", [InputRequired(), Length(min=1, max=144, message=msg_name_length), DataRequired(message="Only using spaces not allowed")])
+    username = StringField("Username", [InputRequired(), Length(min=1, max=144, message=msg_username_length),DataRequired(message="Only using spaces not allowed")])
+    password = PasswordField("Password", [validators.required(), validators.Length(min=2), DataRequired(message="Only using spaces not allowed")])
+    password2 = PasswordField("Password", [validators.required(), validators.Length(min=2), DataRequired(message="Only using spaces not allowed")])
 
     class Meta:
         crsf = False
 
 
 class ChangePasswordForm(FlaskForm):
-    password = PasswordField("New password", [InputRequired(), EqualTo('confirm', message=msg_password_match)])
+    password = PasswordField("New password", [InputRequired(), EqualTo('confirm', message=msg_password_match), DataRequired(message="Only using spaces not allowed")])
     confirm = PasswordField("Repeat new password")
 
     class Meta:
