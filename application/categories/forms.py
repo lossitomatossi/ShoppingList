6084be635f1031dd816b00c3fca6a5d656a2b033
lossitomatossi <tomas.terala@gmail.com>
2019-10-18 12:15:55 +0300

    category/new validointeja ja virheiden näyttö

--- application/categories/forms.py
+++ application/categories/forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, validators
-from wtforms.validators import ValidationError, InputRequired, EqualTo, Length
+from wtforms.validators import ValidationError, InputRequired, EqualTo, Length, DataRequired
 from application.utils.errormessages import msg_name_length
 
 
 class CategoryForm(FlaskForm):
-    name = StringField("Category name", [InputRequired(), Length(min=1,max=144, message=msg_name_length)])
+    name = StringField("Category name", [InputRequired(), Length(min=1,max=144, message=msg_name_length), DataRequired(message="Only using spaces not allowed")])
 
     class Meta:
         csrf = False
